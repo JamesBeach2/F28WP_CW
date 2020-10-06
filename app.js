8d455354788d8556fbccdea471ad664445917c20
JamesBeach2 <jb75@hw.ac.uk>
2020-10-06 14:20:28 +0100

    Fixed comments in app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
+
+// preventing clients from gaining access to any files that aren't in client folder
 app.get('/' , function(req,res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
