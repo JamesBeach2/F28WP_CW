8563d366560fc238ab70f942172a912de06543eb
JamesBeach2 <jb75@hw.ac.uk>
2020-10-06 14:18:03 +0100

    Import express.js in app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// importing express
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/' , function(req,res) {
+	res.sendFile(__dirname + '/client/index.html');
+});
+
+app.use('/client' , express.static(__dirname + '/client'));
+
+// using port 2000
+serv.listen(2000);
