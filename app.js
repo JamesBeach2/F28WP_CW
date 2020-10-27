e393d6b49d05c5977a0aeb905c96a7099f3dab26
JamesBeach2 <jb75@hw.ac.uk>
2020-10-27 19:43:53 +0000

    Update app.js
    Created Player constructor.
    Seperated player and socket data for readability.
    Tried to increase readability by seperating objects

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
-
 // preventing clients from gaining access to any files that aren't in client folder
 app.get('/' , function(req,res)) {
 	res.sendFile(__dirname + '/client/index.html');
 };
-
 app.use('/client' , express.static(__dirname + '/client'));
 
 // Broadcasting on port 2000 and displaying log 
@@ -19,6 +17,11 @@ console.log('server started')
 var socket_list = {};
 var player_list = {};
 
+
+
+
+
+
 // displays an alert in the console when someone attempts to connect to the server
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket)){
@@ -27,15 +30,54 @@ io.sockets.on('connection', function(socket)){
 	socket.id = Math.random();
 	socket_list[socket.id] = socket;
 	
-	//assigning each connection an x and y value
-	socket.x = 0;
-	socket.y = 0;
+	var player = Player[socket.id);
+	player_list[socket.id] = player;
 	
 	console.log('Socket Connection')
+	
+	socket.on('disconnect', function(){
+		console.log('Player disconnected');
+		delete socket_list[socket.id];
+		delete player_list[player.id];
+	
+	}
 };
 
-// looping for every tick
-setInterval (function(){
+
+
+
+
+var Player = function(){
+	var self = {
+		id:id
+		x:500,
+		y:500,
+		speed_x:0,
+		speed_y:0,
+		maxspd:20,
+		accel:2,
+		size:size,
+		sprite: TODO,
+		sprite_colour: TODO
+	}
+	
+	self.update_player = function(){
+	
+	
+	}
+	return self;
+}
+
+
+
+
+
+
+setInterval (function(){		// looping for every tick
+	for(var i in player_list){
+		var player = player_list[i];
+	}
+	
 	for(var i in socket_list){
 		var socket = socket_list[i];
 	}
