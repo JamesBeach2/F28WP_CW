49aab2635f4b06479fd9af521ad58d97207734c5
JamesBeach2 <jb75@hw.ac.uk>
2020-11-08 10:55:19 +0000

    Updated comments
    Explained thought process behind player movement

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ var player_list = {};
 
 
 
-
+// player constructor
+// some values may be unused as of right now
 var Player = function(id){
 	var self = {
 		id: id,
@@ -30,21 +31,32 @@ var Player = function(id){
 		y: Math.floor(Math.random() * 1000),
 		mouseX: 0,
 		mouseY: 0,
-		velocity: 0,
+		//velocity: 0,
 		maxspd:0.1,
-		accel:0.5,
+		//accel:0.5,
 		//self.size = size;
 		//self.sprite = TODO;
 		//self.sprite_colour = TODO;
 	};
 	
-
+	// player movement function
 	self.update_player = function(){
+		// calculating player trajectory based on mouse position
+		// visualised as a triangle where one end of the hypotenuse is the player object and the other is the mouse cursor
+		// xDistance can be visualised as the adjacent line of the triangle and represents the size of the line between x positions of the player and cursor
+		// yDistance can be visualised as the opposite line of the triangle and represents the size of the line between y positions of the player and cursor
 		var xDistance = self.mouseX - self.x;
 		var yDistance = self.mouseY - self.y;
+		// the distance formula: √ (x2-x1)^2+(y2-y1)^2
+		// (x2-x1) and (y2-y1) are represented by xDistance and yDistance
+		// result is the size of the hypotenuse line which is the distance between player object and cursor
 		var distance = Math.sqrt(xDistance * xDistance + yDistance * yDistance);
 		
+		// player positions only change while the mouse is further than 1 unit from the player
+		// this prevents the player from spinning around the mouse
 		if (distance > 1) {
+			// player positions are changed relative to their x-y ratio
+			// maxspd is being temporarily used as a modifier to slow the player down
 			self.x += xDistance * self.maxspd;
 			self.y += yDistance * self.maxspd;
 		}
@@ -63,11 +75,13 @@ io.sockets.on('connection', function(socket){
 	socket.id = Math.random();
 	socket_list[socket.id] = socket;
 	
+	//player is added to the player list
 	var player = Player(socket.id);
 	player_list[socket.id] = player;
 	
 	console.log('Socket Connection');
 	
+	// player and socket info is deleted on disconnect
 	socket.on('disconnect', function(){
 		console.log('Player disconnected');
 		delete socket_list[socket.id];
@@ -75,6 +89,8 @@ io.sockets.on('connection', function(socket){
 	
 	});
 
+	// listener for updates to the mouse position
+	// changes the stored player mouse values accordingly
 	socket.on('update_mouse_pos', function(x,y){
 		player.mouseX = x;
 		player.mouseY = y;
@@ -85,12 +101,15 @@ io.sockets.on('connection', function(socket){
 
 
 setInterval (function(){		// looping for every tick
+	// public player information packet
 	var pack = [];
 	
 	for(var i in player_list){
 		var player = player_list[i];
 
+		// player positions are updated
 		player.update_player();
+		// new player positions are pushed into the packet
 		pack.push({
 			x:player.x,
 			y:player.y
@@ -98,6 +117,7 @@ setInterval (function(){		// looping for every tick
 	}
 	for(var i in socket_list){
 		var socket = socket_list[i];
+		// the packet is emitted to each client
 		socket.emit('update_positions', pack);
 	}
 	
